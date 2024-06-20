88f964ed36c1f0d20d68ad3f2e697425e941ab7d
Jeni1336 <jeniffer1336@gmail.com>
2024-06-19 23:07:51 -0300

    primeiro commit

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Olá React</h1>
+     <p>meu primeiro app em react, eba!!</p>
+    </div>
+  );
+}
+
+export default App;
